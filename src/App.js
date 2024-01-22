eab3fde0e95fd89bb06c10a1ee80f4cdc6591c0a
Enrico de almeida Salomão <enrico.salomao@TINO-C02FT2LDQ05D.local>
2024-01-22 16:35:11 -0300

    teste3

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './routes';
 
 import './styles/global.css'
+import Header from './Components/Header';
 
 
 export const App = () => {
   return (
     <BrowserRouter>
+        <Header/>
         <Routes />
     </BrowserRouter>
   )
