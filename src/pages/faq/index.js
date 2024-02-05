a4c49f5c3ee038e774b5003391bbaeeb4faed960
Enrico de almeida Salomão <enrico.salomao@TINO-C02FT2LDQ05D.local>
2024-02-05 14:20:55 -0300

    faq

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -1,11 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Container } from "./styles";
 
+import SideBar from "../../Components/SideBar";
+
+import { FiChevronRight, FiChevronDown } from "react-icons/fi";
+
 export default function Faq() {
+   const [status, setStatus] = useState(false);
+   const [status2, setStatus2] = useState(false);
+   const [status3, setStatus3] = useState(false);
+   const [status4, setStatus4] = useState(false);
  return (
     <Container>
-        
+      <SideBar/>
+      <div className="content-area">
+         <h1 className="title">FAQ</h1>
+         <h2 className="subTitle">Perguntas frequentes que recebemos ao longo do processo de integração.</h2>
+
+         <div className="answer-area">
+            <div className="header">
+                    {status === true ? 
+                    (<FiChevronDown size={24} color="#fff" onClick={() => setStatus(!status)}/>
+                    ) : (
+                        <FiChevronRight size={24} color="#fff" onClick={() => setStatus(!status)}/>
+                    )}
+
+                    <h1 className="message">
+                     Preciso de algum certificado ou credencial para uso das aplicações?
+                    </h1>
+            </div>
+
+            {status === true && (
+               <h1>Você irá precisar das apiKeys disponibilizada durante o seu cadastro no Tino.</h1>
+            )}
+         </div>
+
+         <div className="answer-area">
+            <div className="header">
+                    {status2 === true ? 
+                    (<FiChevronDown size={24} color="#fff" onClick={() => setStatus2(!status2)}/>
+                    ) : (
+                        <FiChevronRight size={24} color="#fff" onClick={() => setStatus2(!status2)}/>
+                    )}
+
+                    <h1 className="message">
+                    Como faço para solicitar as chaves de acesso?
+                    </h1>
+            </div>
+
+            {status2 === true && (
+               <h1>Você irá recebê-las logo após o seu onboarding com o Tino. Temos um time comercial que irá acompanhá-lo em todo esse processo. Se por algum motivo você não teve acesso às chaves, entre em contato com o representante comercial.</h1>
+            )}
+         </div>
+
+         <div className="answer-area">
+            <div className="header">
+                    {status3 === true ? 
+                    (<FiChevronDown size={24} color="#fff" onClick={() => setStatus3(!status3)}/>
+                    ) : (
+                        <FiChevronRight size={24} color="#fff" onClick={() => setStatus3(!status3)}/>
+                    )}
+
+                    <h1 className="message">
+                    Como faço para acessar os ambientes de staging e production?
+                    </h1>
+            </div>
+
+            {status3 === true && (
+               <>
+                 <h1>SDK: O que diferencia os ambientes no SDK é a apiKey. Você irá receber uma para cada ambiente, mas fazendo a chamada do mesmo javascript.</h1>
+                 <h1>API: O host de staging é o https://stg.supplier-api.truepay.com.br e o de produção é o https://supplier-api.truepay.app, você receberá uma apiKey para cada um destes ambientes (serão keys diferentes das keys do SDK).</h1>
+               </>
+            )}
+         </div>
+
+         <div className="answer-area">
+            <div className="header">
+                    {status4 === true ? 
+                    (<FiChevronDown size={24} color="#fff" onClick={() => setStatus4(!status4)}/>
+                    ) : (
+                        <FiChevronRight size={24} color="#fff" onClick={() => setStatus4(!status4)}/>
+                    )}
+
+                    <h1 className="message">
+                        Como meus lojistas podem ter acesso a sua fatura Tino?
+                    </h1>
+            </div>
+
+            {status4 === true && (
+               <h1>Você pode disponibilizar dentro do seu ambiente para o lojista um redirecionamento para a url app.tino.com.br ou envie através do seu melhor canal de contato..</h1>
+            )}
+         </div>
+      </div>
     </Container>
  );
 }
--- src/pages/faq/styles.js
+++ src/pages/faq/styles.js
@@ -1,3 +1,127 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`   
+    display: flex;
+
+    flex-direction: row;
+
+    .divider{
+            width: 55vw;
+            height: 2px;
+
+            background-color: #2C2E39;
+
+            margin: 1rem 0;
+        }
+
+    .content-area{
+        display: flex;
+
+        padding: 1rem 7%;
+
+        padding-top: 2.5rem;
+
+        flex-direction: column;
+
+        overflow-x: scroll;
+
+        max-height: 84vh;
+
+        .title{
+            font-weight: 500;
+        }
+
+        .subTitle{
+            margin: 1rem 0;
+
+            font-size: 15px;
+
+            font-weight: 200;
+        }
+
+        p{
+            color: #fff;
+
+            margin: .75rem 0;
+
+            width: 55vw;
+
+            font-weight: 200;
+        }
+
+        .video-area{
+            display: flex;
+
+            justify-content: center;
+            align-items: center;
+
+            margin: 1rem 0;
+        }
+
+        .titlep{
+                font-weight: 600;
+
+                font-size: 21px;
+
+                margin-bottom: 0rem;
+        }
+
+        .warn{
+            display: flex;
+
+            margin-top: 1.5rem;
+
+            margin-bottom: 1rem;
+
+            img{
+                width: 48.2vw;
+            }
+        }
+
+        .answer-area{
+            display: flex;
+
+            border: 2px solid #2C2E39;
+
+            flex-direction: column;
+
+            padding: 1rem;
+
+            margin: 1rem 0;
+
+            max-width: 42vw;
+
+            .header{
+                display: flex;
+
+                align-items: center;
+
+                h1{
+                    font-size: 15px;
+
+                    font-weight: 600;
+
+                    padding: .25rem .5rem;
+
+                    margin-left: 0;
+                }
+            }
+
+            h1{
+                font-size: 15px;
+
+                font-weight: 200;
+
+                padding: .25rem .5rem;
+
+                margin-left: 1.5rem;
+
+                max-width: 90%;
+            }
+        }
+    }
+
+    .content-area::-webkit-scrollbar {
+        display: none;
+    }
+`;
