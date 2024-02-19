1db6e8d81a70d3a6d482638d1787bf58c8dab76f
Enrico de almeida Salomão <enrico.salomao@TINO-C02FT2LDQ05D.local>
2024-02-19 15:28:59 -0300

    faq6

--- src/pages/finish/index.js
+++ src/pages/finish/index.js
@@ -33,6 +33,20 @@ export default function Finish() {
             </div>
 
             <img src={warnreser}/>
+
+            <p className="titlep">Callback</p>
+
+            <p style={{ marginTop: '.5rem' }}>Quando a compra for finalizada no widget, vamos redirecionar o lojista para a sua página de sucesso. Essa página será passada via configuração no campo urlCallback.</p>
+
+            <p style={{ marginTop: '.5rem' }}>Além disso, vamos também adicionar 2 parâmetros na sua URL:</p>
+
+            <p style={{ marginTop: '.5rem' }}>- reservationId: ID da reserva criada</p>
+
+            <p style={{ marginTop: '.5rem' }}>- externalId: ID do pedido que foi nos passado via configuração</p>
+
+            <div className="code-area">
+                <h2><text>https://sua-loja.com.br/compra/sucesso?reservationId=53ec5a08-c30e-41b8-949e-fa39dca1572b&externalId=order-19544</text></h2>
+            </div>
         </div>
     </Container>
  );
